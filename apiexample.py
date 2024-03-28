07ea918695eaf5c49d5514b9ab157d57d369904e
Shain Collins <scolli03.sc@gmail.com>
2024-03-27 19:57:30 -0500

    got time ready for current crimes

--- apiexample.py
+++ apiexample.py
@@ -1,11 +1,18 @@
 import requests
-
+from pprint import pprint
+from datetime import datetime
 
 response = requests.get('https://api.torn.com/faction/?selections=crimes,positions&key=2UQSejH3OfK8S5ne')
 
 responsejson = response.json()
 
-for key in responsejson:
-    print(key)
+crimes = responsejson['crimes']
+
+for key in crimes:
+
+    if crimes[key]['time_left'] > 0:
+        print(datetime.fromtimestamp(int(crimes[key]['time_ready'])))
+
+
 
 
